40f6208bc02fdb47e7ba3939cd1a6f88e8fa600b
trent <trent.shipley@gmail.com>
2022-12-15 12:13:03 -0700

    Almost done with chapter 2?

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,4 @@
-from typing import Self
+#from typing import Self
 from selenium import webdriver
 import unittest
 #from selenium.webdriver.firefox.options import Options
@@ -25,13 +25,14 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # # She goes to check out its homepage.
         self.browser.get('http://localhost:8000')
         
         # She notices the page title and header mention to-do lists
-        assert 'To-Do' in browser.title, "Browser title was " + browser.title
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
         # She is invited to enter a to-do item straight away
 
@@ -62,9 +63,6 @@ class NewVisitorTest(unittest.TestCase):
         # Satisfied, she goes back to sleep
 
 
-        
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
-
-
-
-browser.quit()
