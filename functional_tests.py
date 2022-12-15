cff273bf42a232a816a5ce10a5583ceeb9f91be3
trent <trent.shipley@gmail.com>
2022-12-15 11:47:58 -0700

    cd

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
+from typing import Self
 from selenium import webdriver
+import unittest
 #from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -7,12 +9,62 @@ from webdriver_manager.firefox import GeckoDriverManager
 #options = selenium.webdriver.Options()
 #options.binary_location = executable_path
 #print(options.binary_location)
-driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+#driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+#browser = driver
 #driver.get('http://google.com/')
 
 #browser = webdriver.Firefox()
 #browser = webdriver.Firefox(options = options)
 #browser = webdriver.Firefox(executable_path=executable_path)
-driver.get('http://127.0.0.1:8000')
 
-assert 'Django' in browser.title
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+        
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrive_it_later(self):
+        # Edith has heard about a cool new online to-do app.
+        # # She goes to check out its homepage.
+        self.browser.get('http://localhost:8000')
+        
+        # She notices the page title and header mention to-do lists
+        assert 'To-Do' in browser.title, "Browser title was " + browser.title
+
+        # She is invited to enter a to-do item straight away
+
+
+        # She types "Buy peacock feather" into a text box.
+        # (Edith's hobby is tying fly-fishing lures.)
+
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in the to-do list.
+
+
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers tto make a fly" (Edith is very methodical.)
+
+
+        # The page updates again, and now shows both items on her list.
+
+
+        # Edith wonder wether the site will remember her list.
+        # Then she sees that the site has generated a unique URL for her --
+        # there is some explanatory text to that effect.
+
+
+        # She visit that URL - her to-do list is still there.
+
+
+        # Satisfied, she goes back to sleep
+
+
+        
+
+
+
+
+browser.quit()
