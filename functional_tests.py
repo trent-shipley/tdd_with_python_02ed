8d74440aaf7310f2e9d031e14d8de60b6dd60591
trent <trent.shipley@gmail.com>
2022-12-20 09:05:41 -0700

    Front page HTML now generated from a template.  Selenium syntax for locating elements has changed.

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 #from typing import Self
 from selenium import webdriver
+from selenium.webdriver.common.by import By  # Change in Ch4 
 #from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.firefox.service import Service as FirefoxService
 from webdriver_manager.firefox import GeckoDriverManager
@@ -34,11 +35,12 @@ class NewVisitorTest(unittest.TestCase):
         
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text
+#        header_text = self.browser.find_element_by_tag_name('h1').text  Not working 2022-12-20
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text # Selenium update
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')  # Selenium update
         self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
         
 
@@ -52,10 +54,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
         
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        table = self.browser.find_element(
+            By.ID, 'id_list_table')  # Selenium update
+        rows = table.find_elements(By.TAG_NAME, 'tr')  # Selenium update
         self.assertTrue(
-            any(row.next=='1: Buy peacock feathers' for row in rows)
+            any(row.next=='1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table"
         )
         
         # There is still a text box inviting her to add another item.
