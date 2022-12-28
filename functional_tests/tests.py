d00181d644e8dd20047a6f64c6f10b38294f5979
trent <trent.shipley@gmail.com>
2022-12-28 10:24:38 -0700

    make functional_tests app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+from django.test import LiveServerTestCase
 #from typing import Self
 from selenium import webdriver
 from selenium.webdriver.common.by import By  # Change in Ch4 
@@ -8,20 +9,9 @@ from selenium.webdriver.common.keys import Keys
 import time
 import unittest
 
-#executable_path = r"C:\Users\Trent\AppData\Local\Mozilla Firefox\firefox.exe"
-#options = selenium.webdriver.Options()
-#options.binary_location = executable_path
-#print(options.binary_location)
-#driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-#browser = driver
-#driver.get('http://google.com/')
-
-#browser = webdriver.Firefox()
-#browser = webdriver.Firefox(options = options)
-#browser = webdriver.Firefox(executable_path=executable_path)
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
         
@@ -36,7 +26,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app.
         # # She goes to check out its homepage.
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
