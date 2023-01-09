e8a06097caa9ba51eeeb3f6fb56bf1989994ce59
trent <trent.shipley@gmail.com>
2023-01-09 09:30:57 -0700

    Got code mostly working still one bug to hunt down to match text

--- superlists/urls.py
+++ superlists/urls.py
@@ -19,4 +19,5 @@ from lists import views
 
 urlpatterns = [
     re_path(r'^^$', views.home_page, name='home'),
+    re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
 ]
