dcc3ddb0d34483bcec032b3ad8186704beec5a16
trent <trent.shipley@gmail.com>
2023-01-11 10:21:38 -0700

    Got the test to work.  There was lots of hunting spelling errors.

--- superlists/urls.py
+++ superlists/urls.py
@@ -18,6 +18,6 @@ from django.urls import re_path
 from lists import views
 
 urlpatterns = [
-    re_path(r'^^$', views.home_page, name='home'),
+    re_path(r'^$', views.home_page, name='home'),
     re_path(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
 ]
