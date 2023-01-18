80059c451b7293981a4097879a3b5103dd77a3a4
trent <trent.shipley@gmail.com>
2023-01-18 07:22:00 -0700

    Altered model to have list of items.,

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
 # from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
+
+
 
 # Create your views here.
 
@@ -15,5 +17,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-list-in-the-world/')
